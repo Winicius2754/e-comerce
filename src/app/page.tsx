d2a1cff8e8df4f3f2364704a0cfcb831c0c496e0
Winicius2754 <winiciusneveswn@gmail.com>
2025-04-17 08:28:40 -0300

    criando lista de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@
 export default function Home() {
   return (
     
-      <div>
-      <h1>Home</h1>
+      <div className='max-w-7xl mx-auto pt-8 px-8 px-0'>
+        <div className="grid grid-cols-1 sm:grid-cols-2 mg:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+          <div className=" bg-white rounded-lg shadow-lg overflow-hidden">product</div>
+          <div className=" bg-white rounded-lg shadow-lg overflow-hidden">product</div>
+          <div className=" bg-white rounded-lg shadow-lg overflow-hidden">product</div>
+          <div className=" bg-white rounded-lg shadow-lg overflow-hidden">product</div>
+         
+        </div>
+      
       </div>
     
   );
